46d750daeb97f0ba2638d12821051d2cdf3aa62d
Marc Evanstein <marc@marcevanstein.com>
2024-01-12 14:17:24 -0800

    Cleanup, added find_strategy_overlap.py and draw_game_states.py for generating visuals for video

--- old/Countdonwn.py
+++
@@ -1,9 +0,0 @@
-from scamp import *
-
-s = Session()
-
-def on_move(x, y):
-    value = int(x * 40000 if x * 40000 < 31896 else 31896)
-    print(f'{value:,}')
-
-s.register_mouse_listener(on_move=on_move, relative_coordinates=True)
